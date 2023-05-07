9073be48c0e40539d88134a04d786453943f1fad
GrenManSK <melnicek.richard@gmail.com>
2023-05-07 16:40:35 +0200

    Update __get_index__.py

--- __get_index__.py
+++ __get_index__.py
@@ -4,14 +4,19 @@ def get_index():
     info = p.get_host_api_info_by_index(0)
     numdevices = info.get('deviceCount')
 
+    pocet = 0
     for i in range(0, numdevices):
         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            pocet += 1
             print("Input Device id ", i, " - ",
                 p.get_device_info_by_host_api_device_index(0, i).get('name'))
             
     while True:
         try:
             vstup = int(input('Select your default input device > '))
+            if vstup >= pocet:
+                print('Out of range')
+                continue
             break
         except ValueError:
             continue
